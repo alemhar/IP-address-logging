3b02b01f2f8c9ff706ac284128ac201c31659321
Alemhar <alemhar@gmail.com>
2021-11-06 21:31:32 +0800

    created migration and Models IP/Label

---
+++ database/migrations/2021_11_06_132442_create_labels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ip_id');
+            $table->foreignId('user_id');
+            $table->string('action');
+            $table->string('label');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+}
